b03809aeda9536fa5b46d95dc75f4589244fd781
Alexander-Snigirev <144445249+Alexander-Snigirev@users.noreply.github.com>
2025-03-07 13:44:48 +0300

    Crugovaya_orbita

--- t.py
+++ t.py
@@ -1,26 +1,46 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import math
 
 
 G = 6.67430e-11  
-M_p = 5.972e26 
+M_p = 5.972e24
 M_s = 1000  
 r_p = 6.371e6
+orbit_radius = 1.5e9
+angle = 180
 
 
 x_p, y_p = 0, 0
-v_px, v_py = 0, 0.5e3
-x_s, y_s = -4e8, 6e7
-v_x, v_y = 7e4, 8e3
-dt = 100
-num_steps = 7850
 
+
+v_px, v_py = 0, 0.5e3
+v_p = 2.9e4
+x_star, y_star = x_p + orbit_radius, 0 
+
+x_s, y_s = 3 * r_p, -4 * r_p  # Initial position of the spacecraft
+v_x, v_y = -7e3, 3e3
+dt = 60
+day = 1440
+num_steps = day//10 # 1 сутки
+T = 360 * 24 * 3600  # Полный оборот за 360 дней в секундах
+omega = 2 * np.pi / T  # Угловая скорость (рад/сек)
+
+def PlanetMoving(t):
+    
+    x_p = -orbit_radius * np.cos(omega * t) + orbit_radius
+    y_p = orbit_radius * np.sin(omega * t)
+    
+    return x_p, y_p
 
 initial_distance = np.sqrt((x_s - x_p)**2 + (y_s - y_p)**2)
+
+
 def EulerMethod(x_s, y_s, x_p, y_p, v_x, v_y):
     x_trajectory, y_trajectory = [], []
     x_planet_trajectory, y_planet_trajectory = [], []
     speed, distance = [], []
+    t=0
     for _ in range(num_steps):
         r = np.sqrt((x_s - x_p)**2 + (y_s - y_p)**2)
         if r <= r_p:
@@ -38,8 +58,9 @@ def EulerMethod(x_s, y_s, x_p, y_p, v_x, v_y):
         
         x_s += v_x * dt
         y_s += v_y * dt
-        x_p += v_px * dt
-        y_p += v_py * dt
+        x_p, y_p = PlanetMoving(t)
+        #x_p += v_px * dt
+        #y_p += v_py * dt
 
         
         x_trajectory.append(x_s)
@@ -50,12 +71,15 @@ def EulerMethod(x_s, y_s, x_p, y_p, v_x, v_y):
         
         speed.append(np.sqrt(v_x**2 + v_y**2))
         distance.append(r)
+        t+=dt
     return x_trajectory, y_trajectory, x_planet_trajectory, y_planet_trajectory, speed, distance     
 
 def RK4Method(x_s, y_s, x_p, y_p, v_x, v_y):
     x_trajectory, y_trajectory = [], []
     x_planet_trajectory, y_planet_trajectory = [], []
     speed, distance = [], []
+    t = 0
+    ang = angle
 
     def acceleration(x_s, y_s, x_p, y_p):
         r = np.sqrt((x_s - x_p)**2 + (y_s - y_p)**2)
@@ -78,30 +102,33 @@ def RK4Method(x_s, y_s, x_p, y_p, v_x, v_y):
 
         # K2 (используем половину шага)
         a_x2, a_y2 = acceleration(x_s + 0.5 * k1_x, y_s + 0.5 * k1_y, x_p, y_p)
+        if a_x2 is None: break  # Остановка при столкновении
         k2_vx, k2_vy = a_x2 * dt, a_y2 * dt
         k2_x, k2_y = (v_x + 0.5 * k1_vx) * dt, (v_y + 0.5 * k1_vy) * dt
 
         # K3 (ещё одна половина шага)
         a_x3, a_y3 = acceleration(x_s + 0.5 * k2_x, y_s + 0.5 * k2_y, x_p, y_p)
+        if a_x3 is None: break  # Остановка при столкновении
         k3_vx, k3_vy = a_x3 * dt, a_y3 * dt
         k3_x, k3_y = (v_x + 0.5 * k2_vx) * dt, (v_y + 0.5 * k2_vy) * dt
 
         # K4 (полный шаг)
+        
         a_x4, a_y4 = acceleration(x_s + k3_x, y_s + k3_y, x_p, y_p)
+        if a_x4 is None: break  # Остановка при столкновении
         k4_vx, k4_vy = a_x4 * dt, a_y4 * dt
         k4_x, k4_y = (v_x + k3_vx) * dt, (v_y + k3_vy) * dt
 
         # Обновляем скорость спутника
         v_x += (k1_vx + 2 * k2_vx + 2 * k3_vx + k4_vx) / 6
         v_y += (k1_vy + 2 * k2_vy + 2 * k3_vy + k4_vy) / 6
-
+        
         # Обновляем координаты спутника
         x_s += (k1_x + 2 * k2_x + 2 * k3_x + k4_x) / 6
         y_s += (k1_y + 2 * k2_y + 2 * k3_y + k4_y) / 6
 
-        # Обновляем координаты планеты (движется равномерно)
-        x_p += v_px * dt
-        y_p += v_py * dt
+        x_p, y_p = PlanetMoving(t)
+
 
         # Сохраняем данные для визуализации
         x_trajectory.append(x_s)
@@ -110,8 +137,11 @@ def RK4Method(x_s, y_s, x_p, y_p, v_x, v_y):
         y_planet_trajectory.append(y_p)
         speed.append(np.sqrt(v_x**2 + v_y**2))
         distance.append(np.sqrt((x_s - x_p)**2 + (y_s - y_p)**2))
+        t += dt
 
     return x_trajectory, y_trajectory, x_planet_trajectory, y_planet_trajectory, speed, distance
+
+
 def draw_nahui(x_trajectory, y_trajectory, x_planet_trajectory, y_planet_trajectory, speed, distance):
     plot_limit = 2 * initial_distance
     fig1, ax1 = plt.subplots(figsize=(10, 10))
@@ -119,6 +149,7 @@ def draw_nahui(x_trajectory, y_trajectory, x_planet_trajectory, y_planet_traject
     ax1.plot(x_planet_trajectory, y_planet_trajectory, label='Planet Trajectory', linestyle='--')
     ax1.plot(x_p, y_p, 'ro', markersize=10, label='Planet')  
     ax1.plot(x_trajectory[0], y_trajectory[0], 'go', markersize=8, label='Start Point')  
+    ax1.plot(x_star, y_star, 'yo', markersize=15, label = "Sun")
     ax1.set_aspect('equal', adjustable='box')
     ax1.set_xlabel('X coordinate (m)')
     ax1.set_ylabel('Y coordinate (m)')
